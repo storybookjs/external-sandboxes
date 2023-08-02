603c1fb15c006cc004291a644828d204941496f2
Yann Braga <yannbf@gmail.com>
2023-08-02 15:34:57 +0200

    add test-runner

--- yarn-workspaces-monorepo/packages/storybook-app/.storybook/main.ts
+++ yarn-workspaces-monorepo/packages/storybook-app/.storybook/main.ts
@@ -1,21 +1,25 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ['../../react-vite/src/**/*.mdx', '../../react-vite/src/**/*.stories.@(js|jsx|mjs|ts|tsx)', {
-    directory: '../src/stories'
-  }],
+  stories: [
+    "../../react-vite/src/**/*.mdx",
+    "../../react-vite/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    {
+      directory: "../src/stories",
+    },
+  ],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: "@storybook/react-vite",
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
 };
 export default config;
